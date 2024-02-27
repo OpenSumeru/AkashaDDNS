3e7990cf739e361b18ed5b7cedae5882d88cd3d8
Tab Ye <tabye320@outlook.com>
2024-02-28 01:03:32 +0800

    add move

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,39 @@
 
 int main(size_t argc, char** argv)
 {
+    if (argc < 2)
+    {
+        std::cout << "Too few parameters" << std::endl;
+        return -1;
+    }
     std::string configPath = argv[1];
-    auto client = std::make_shared<httplib::Client>("https://api.cloudflare.com");
+    httplib::Client client("https://api.cloudflare.com");
     if (configPath.length() == 0)
     {
         configPath = "config.json";
     }
     auto config = readFileToJson(configPath);
-    verifyAPI(*client, config["Headers"]);
-    auto zoneId = findZoneId(*client, config["Zone"]);
+    verifyAPI(client, config["Headers"]);
+    auto&& zoneId = findZoneId(client, config["Zone"]);
     if (zoneId.length() == 0)
     {
         std::cout << "Find Zone ID Error" << std::endl;
         return 0;
     }
-    auto dnsName = config["Target"]["DNS-Name"].string_value();
-    auto dnsRecordId = findRecordId(*client, zoneId, dnsName);
+    auto& dnsName = config["Target"]["DNS-Name"].string_value();
+    auto&& dnsRecordId = findRecordId(client, zoneId, dnsName);
     if (dnsRecordId.length() == 0)
     {
         std::cout << "DNS Record ID Error" << std::endl;
         return 0;
     }
-    auto ipAddress = getResult(*client, "/zones/" + zoneId + "/dns_records/" + dnsRecordId)["content"].string_value();
+    auto ipAddress = getResult(client, "/zones/" + zoneId + "/dns_records/" + dnsRecordId)["content"].string_value();
     while (true)
     {
-        auto rax = getIPv4();
+        auto&& rax = getIPv4();
         if (rax != ipAddress)
         {
-            auto reciveIp = putRecordId(*client, zoneId, dnsRecordId, rax, dnsName);
+            auto&& reciveIp = putRecordId(client, zoneId, dnsRecordId, rax, dnsName);
             if (reciveIp == rax)
             {
                 ipAddress = rax;
