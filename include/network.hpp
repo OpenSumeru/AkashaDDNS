d76a78e3dd971817707058fe1d2752720092e177
Tab Ye <tabye320@outlook.com>
2024-03-08 00:32:46 +0800

    bug fix 2

--- include/network.hpp
+++ include/network.hpp
@@ -4,6 +4,10 @@
 inline std::string getIPv4()
 {
     static httplib::Client client("https://api.ipify.org");
-    auto ip = client.Get("/")->body;
-    return ip;
+    auto&& result = safeGet(client, "/");
+    if (result->status != StatusCode::OK_200)
+    {
+        return "";
+    }
+    return std::string(result->body);
 }
