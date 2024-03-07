c34ae9463d394bbe48cd68e4601b3098abe6eead
twhice <twhice@outlook.com>
2024-03-08 01:15:53 +0800

    跟进bug fix2...算了直接移除cpp部分

--- cpp/include/network.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-
-inline std::string getIPv4()
-{
-    static httplib::Client client("https://api.ipify.org");
-    auto ip = client.Get("/")->body;
-    return ip;
-}
