5312e5479b196c4137c0d933ba424eba7a22a2dc
Tab Ye <tabye320@outlook.com>
2024-05-24 00:52:52 +0800

    Fix OpenSSL 1.1.1

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
 add_languages("cxx20")
-add_requires("nlohmann_json", "openssl3", "cpp-httplib")
+add_requires("nlohmann_json", "openssl3", "cpp-httplib", {config = ssl})
 
 target("AkashaDDNS")
     add_includedirs("include")
     add_files("src/*.cpp")
     add_headerfiles("include/*.h", "include/*.hpp")
-    add_packages("nlohmann_json", "openssl3", "cpp-httplib")
+    add_packages("nlohmann_json", "openssl3", "cpp-httplib", {config = ssl})
 target_end()
