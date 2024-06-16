7b71c4e1c358a9661f3fc82df74b18da3707acf6
Tab Ye <tabye320@outlook.com>
2024-06-16 20:57:26 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,10 @@
 add_languages("cxx20")
-add_requires("nlohmann_json", "openssl3", "cpp-httplib", {config = {ssl = true}})
+add_requires("nlohmann_json", "cpp-httplib", {config = {ssl = true}})
 
 target("AkashaDDNS")
     add_includedirs("include")
     add_files("src/*.cpp")
     add_headerfiles("include/*.h", "include/*.hpp")
     set_pcxxheader("include/pch.h")
-    add_packages("nlohmann_json", "openssl3", "cpp-httplib", {config = {ssl = true}})
+    add_packages("nlohmann_json", "cpp-httplib", {config = {ssl = true}})
 target_end()
